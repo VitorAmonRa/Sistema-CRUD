8b10890d96b132dea8344b7ba0fdb1eda7e8780c
Vitor Amon-Ra Fernandes <vitoramon-ra@hotmail.com>
2022-11-22 20:44:36 -0300

    Implementado sistema de login via FireBase

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,5 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
-
+import { signOut } from "firebase/auth";
+import { auth } from '../../services/firebaseConnection'
+import {useNavigate } from 'react-router-dom';
 import Global from '../../GlobalStyle/Global';
 
 import { 
@@ -7,11 +9,25 @@ import {
 } from './style';
 
 export const HomePage: React.FC = () => {
+  const navigate = useNavigate()
+
+  function handleLogOut() {
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      navigate("/login")
+    }).catch((error) => {
+      // An error happened.
+    });
+    
+  }
+
 
   return(
     <>
     <Global />
         <Container>
+          <div>Pagina HOME</div>
+          <button onClick={() => handleLogOut()}></button>
         </Container>
     </>
   );
