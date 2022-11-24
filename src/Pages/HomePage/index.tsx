b358f35f06448035fc1c80c173203948177bcad4
Vitor Amon-Ra Fernandes <vitoramon-ra@hotmail.com>
2022-11-24 23:58:20 +0000

    Update 4 files

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,37 +1,16 @@
-import React from 'react';
-import { signOut } from "firebase/auth";
-import { auth } from '../../services/firebaseConnection'
-import {useNavigate } from 'react-router-dom';
-import Global from '../../GlobalStyle/Global';
+import React from "react";
+import Global from "../../GlobalStyle/Global";
 
-import { 
-  Container,
-} from './style';
-import Navbar from '../../Components/Navbar';
+import { Container } from "./style";
+import Navbar from "../../Components/Navbar";
 
 export const HomePage: React.FC = () => {
-  const navigate = useNavigate()
-
-  function handleLogOut() {
-    signOut(auth).then(() => {
-      // Sign-out successful.
-      navigate("/login")
-    }).catch((error) => {
-      // An error happened.
-    });
-    
-  }
-
-
-  return(
+  return (
     <>
-    <Global />
-        <Container>
-          <Navbar/>
-          <button onClick={() => handleLogOut()}></button>
-        </Container>
+      <Global />
+      <Container>
+        <Navbar />
+      </Container>
     </>
   );
-}
-
-
+};
