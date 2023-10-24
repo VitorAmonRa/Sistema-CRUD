cf42b2df0ecab08d9312b64efed33575bdd19775
Vitor Amon-Ra Fernandes <vitoramon-ra@hotmail.com>
2023-10-24 12:15:28 -0300

    Criando banco de dados

---
+++ server/src/router.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const tasksController = require("./controllers/tasksController");
+const router = express.Router();
+
+router.get("/tasks", tasksController.getAll);
+module.exports = router;
