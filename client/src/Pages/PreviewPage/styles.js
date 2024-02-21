c819e7d2677a94ae77a52a70ba410ba0446f0029
Vitor Amon-Ra Fernandes <91434756+VitorAmonRa@users.noreply.github.com>
2024-02-21 17:30:50 -0300

    Update styles.js

--- client/src/Pages/PreviewPage/styles.js
+++ client/src/Pages/PreviewPage/styles.js
@@ -34,7 +34,7 @@ export const SituationOfEquipments = styled.div`
   flex-direction: column;
   align-items: center;
   width: 100%;
-  max-width: 440px;
+  max-width: 380px;
   height: min-content;
   opacity: 1;
   background-color: rgba(0, 0, 0, 0.4);
